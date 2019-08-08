b4e776296620183aefcc846157ab4dc3b5e67080
Dorota Adamska <dorota.sobel@gmail.com>
2019-08-08 11:49:37 +0200

    Add tiny-slider functionallity in app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,30 @@
-console.log('hello');
+import { tns } from '../../node_modules/tiny-slider/src/tiny-slider';
+
+/* NEW FURNITURE - slider */
+let furnitureSlider = tns({
+  container: '.my-slider',
+  items: 1,
+  slideBy: 'page',
+  loop: true,
+  mouseDrag: true,
+  speed: 4000,
+  navContainer: '.product-dots',
+  controls: false,
+  controlsText: false,
+  swipeAngle: false,
+  responsive: {
+    576: {
+      items: 2
+    },
+    768: {
+      items: 2
+    },
+    992: {
+      items: 4
+    }
+  }
+});
+
+window.addEventListener('resize', function () {
+  furnitureSlider.rebuild();
+});
