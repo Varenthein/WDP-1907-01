12c91a879471226e3f1a0a828fa16f42246ab35a
Dorota Adamska <dorota.sobel@gmail.com>
2019-08-15 20:16:29 +0200

    Optimize code in slider.js

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,9 +1,7 @@
 import { tns } from '../../node_modules/tiny-slider/src/tiny-slider';
 
 /* NEW FURNITURE - slider */
-
-tns({
-  container: '.bed-slider',
+const slider = {
   items: 1,
   slideBy: 'page',
   speed: 1000,
@@ -27,112 +25,23 @@ tns({
       items: 4
     }
   }
+};
+tns({
+  container: '.bed-slider',
+  ...slider
 });
 
 tns({
   container: '.chair-slider',
-  items: 1,
-  slideBy: 'page',
-  speed: 1000,
-  controls: false,
-  autoplay: false,
-  autoplayButtonOutput: false,
-  preventScrollOnTouch: 'auto',
-  gutter: 5,
-  mouseDrag: true,
-  responsive: {
-    0: {
-      items: 1
-    },
-    576: {
-      items: 2
-    },
-    768: {
-      items: 3
-    },
-    992: {
-      items: 4
-    }
-  }
-});
-
-tns({
-  container: '.sofa-slider',
-  items: 1,
-  slideBy: 'page',
-  speed: 1000,
-  controls: false,
-  autoplay: false,
-  autoplayButtonOutput: false,
-  preventScrollOnTouch: 'auto',
-  gutter: 5,
-  mouseDrag: true,
-  responsive: {
-    0: {
-      items: 1
-    },
-    576: {
-      items: 2
-    },
-    768: {
-      items: 3
-    },
-    992: {
-      items: 4
-    }
-  }
+  ...slider
 });
 
 tns({
   container: '.table-slider',
-  items: 1,
-  slideBy: 'page',
-  speed: 1000,
-  controls: false,
-  autoplay: false,
-  autoplayButtonOutput: false,
-  preventScrollOnTouch: 'auto',
-  gutter: 5,
-  mouseDrag: true,
-  responsive: {
-    0: {
-      items: 1
-    },
-    576: {
-      items: 2
-    },
-    768: {
-      items: 3
-    },
-    992: {
-      items: 4
-    }
-  }
+  ...slider
 });
 
 tns({
   container: '.dining-slider',
-  items: 1,
-  slideBy: 'page',
-  speed: 1000,
-  controls: false,
-  autoplay: false,
-  autoplayButtonOutput: false,
-  preventScrollOnTouch: 'auto',
-  gutter: 5,
-  mouseDrag: true,
-  responsive: {
-    0: {
-      items: 1
-    },
-    576: {
-      items: 2
-    },
-    768: {
-      items: 3
-    },
-    992: {
-      items: 4
-    }
-  }
+  ...slider
 });
