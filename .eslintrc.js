847ebde4cfefde4f2ef00714a819f15c2f810d55
Dorota Adamska <dorota.sobel@gmail.com>
2019-08-16 19:52:04 +0200

    change .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
-  "extends": "standard",
-  "parserOptions": {
-    "ecmaVersion": 5
+  extends: "standard",
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 5
   },
-  "rules": {
+  rules: {
     "no-console": ["warn"],
     "no-debugger": ["error"],
-    "semi": [2, "always"]
+    semi: [2, "always"]
   }
 };
