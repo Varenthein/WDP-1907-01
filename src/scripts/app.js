7e9d63d6009e9aa228fb0ddc9bef74466d5c76eb
Marcin Kaleta <kalet1984@gmail.com>
2019-08-21 15:40:46 +0200

    Add requested changes

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,13 +2,26 @@
 
 const starsPan = document.querySelectorAll('.stars'); // select all stars panels
 
+function addStarClass(currentIndex, array) {
+  //add 'full' class to star
+  for (let i = 0; i <= currentIndex; i++) {
+    array[i].classList.add('full');
+  }
+}
+
+function removeStarClass(currentIndex, array) {
+  //remove 'full' class from star
+  for (let i = currentIndex + 1; i < array.length; i++) {
+    array[i].classList.remove('full');
+  }
+}
+
 for (let panel of starsPan) {
   panel.addEventListener('click', e => {
     e.preventDefault(); // prevent target acting like a link
-    console.log('click');
     if (e.target.tagName === 'A') {
-      var allStars = panel.querySelectorAll('a'); // select all stars in panel
-      var allStarsArr = [...allStars]; // make array from allStars
+      let allStars = panel.querySelectorAll('a'); // select all stars in panel
+      let allStarsArr = [...allStars]; // make array from allStars
       const starIndex = allStarsArr.indexOf(e.target); // selected star
 
       for (let i = 0; i <= starIndex; i++) {
@@ -23,31 +36,19 @@ for (let panel of starsPan) {
 
   panel.addEventListener('mouseover', e => {
     if (e.target.tagName === 'A') {
-      var allStars = panel.querySelectorAll('a');
-      var allStarsArr = [...allStars];
+      let allStars = panel.querySelectorAll('a');
+      let allStarsArr = [...allStars];
       const starIndex = allStarsArr.indexOf(e.target);
-  
-      for (let i = 0; i <= starIndex; i++) {
-        allStarsArr[i].classList.add('full');
-      }
-
-      for (let i = starIndex + 1; i < allStarsArr.length; i++) {
-        allStarsArr[i].classList.remove('full');
-      }
+      addStarClass(starIndex, allStarsArr);
+      removeStarClass(starIndex, allStarsArr);
     }
   });
 
   panel.addEventListener('mouseleave', e => {
-    var allStars = panel.querySelectorAll('a');
-    console.log(allStars);
-    var allStarsArr = [...allStars];
-    console.log(allStarsArr);
+    let allStars = panel.querySelectorAll('a');
+    let allStarsArr = [...allStars];
     const starIndex = allStarsArr.indexOf(e.target);
-    console.log(starIndex);
-
-    for (let i = 0; i < allStarsArr.length; i++) {
-      allStarsArr[i].classList.remove('full');
-    }
+    removeStarClass(starIndex, allStarsArr);
   });
 }
 
